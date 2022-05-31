4d07a9c6b9837eab8e6e74fd73257df8197a697c
brandonjustin <98418527+brandonjustin@users.noreply.github.com>
2022-05-31 01:38:36 +0100

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,11 @@
+ ndocument.getElementById("btn").addEventListener("click",function(){
+
+var num = 0;
+var value = document.getElementById("card").value;
+
+while(value==0){
+    value = value++;
+}
+
+
+});
